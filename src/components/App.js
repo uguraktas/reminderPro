1f870d79d9f2879abce5020aa7ea6d7f1452b64d
Uğur AKTAŞ <uguraktas25@yandex.com>
2018-09-16 00:25:48 +0300

    first blood

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="h5">Reminder Pro</div>
+          <hr />
+          <div className="form-inline">
+            <input className="form-control" placeholder="Yapılacak birşeyler ekle..." />
+            <button type="button" className="btn btn-success mx-2">Ekle</button>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default App;

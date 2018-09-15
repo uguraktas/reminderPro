4bfdaed9c697a1670f715e939265dd840ea22028
Uğur AKTAŞ <uguraktas25@yandex.com>
2018-09-16 00:58:14 +0300

    third

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
 import App from './components/App'
+
+const store = createStore(reducer);
+
 
 console.log('çalıştımı sdsd')
 
-ReactDOM.render( 
-  <App/>, document.getElementById('root')
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
 )
